9dd2e40d42de0b8a48ce19a46077d38746929c99
Manop <manoprr@gmail.com>
2018-09-22 15:51:58 +0700

    finnish 22 sep

--- app/src/main/java/manop/mytutor/com/mytutor/fragment/LoginFragment.java
+++ app/src/main/java/manop/mytutor/com/mytutor/fragment/LoginFragment.java
@@ -7,9 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import manop.mytutor.com.mytutor.R;
+import manop.mytutor.com.mytutor.utility.MyAlert;
 
 public class LoginFragment extends Fragment{
 
@@ -18,6 +26,60 @@ public class LoginFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Register Controller
+        registerController();
+
+//        SignIn Controller
+        signInController();
+
+
+    }   // main method
+
+    private void signInController() {
+        Button button = getView().findViewById(R.id.btnSignIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+                final MyAlert myAlert = new MyAlert(getActivity());
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (emailString.isEmpty() || passwordString.isEmpty() ) {
+                    myAlert.normalDialog(getString(R.string.have_space), getString(R.string.message_space));
+
+                } else {
+
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+
+                                        getActivity().getSupportFragmentManager().popBackStack();
+
+                                    } else {
+
+                                        myAlert.normalDialog("Cannot Sign IN",
+                                                task.getException().getMessage().toString());
+                                    }
+
+                                }
+                            });
+
+
+                }   //if
+
+
+            }
+        });
+    }
+
+    private void registerController() {
         TextView textView = getView().findViewById(R.id.txtRegister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +93,6 @@ public class LoginFragment extends Fragment{
 
             }
         });
-
     }
 
     @Nullable
--- app/src/main/java/manop/mytutor/com/mytutor/fragment/MainFragment.java
+++ app/src/main/java/manop/mytutor/com/mytutor/fragment/MainFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +15,25 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import manop.mytutor.com.mytutor.R;
+import manop.mytutor.com.mytutor.utility.BannerAdepter;
+import manop.mytutor.com.mytutor.utility.BannnerModel;
+import manop.mytutor.com.mytutor.utility.OnClickItem;
 
 public class MainFragment extends Fragment {
 
 
     private TextView textView;
+    private boolean aBoolean = true;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -28,33 +43,83 @@ public class MainFragment extends Fragment {
         loginController();
 
 //        Show DisplayName
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        showDisplayName();
 
-        if (firebaseUser != null) {
-            textView.setText(firebaseUser.getDisplayName());
+//        Banner RecyclerView
+        bannerRecyclerView();
 
-        }
 
-//        Acount Click
-        textView = getView().findViewById(R.id.btnAccount);
-        textView.setOnClickListener(new View.OnClickListener() {
+    }   //Main Method
+
+    private void bannerRecyclerView() {
+        final RecyclerView recyclerView = getView().findViewById(R.id.recyclerBanner);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+//        Read Firebase Database
+        final int[] timesInts = new int[]{0};
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("Banner");
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                getActivity()
-                        .getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.contentMainFragment, new StudentProfileFragment())
-                        .addToBackStack(null)
-                        .commit();
+                int i = (int) dataSnapshot.getChildrenCount();
+                ArrayList<String> stringArrayLis = new ArrayList<>();
+                final ArrayList<String> uidStringArrayList = new ArrayList<>();
+                List list = new ArrayList<>();
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+
+                    BannnerModel bannnerModel = dataSnapshot1.getValue(BannnerModel.class);
+                    list.add(bannnerModel);
+
+                    BannnerModel bannnerModel1 = (BannnerModel) list.get(timesInts[0]);
+                    stringArrayLis.add(bannnerModel1.getImage());
+                    uidStringArrayList.add(bannnerModel1.getUidString());
+                    timesInts[0] += 1;
+
+                }
+                Log.d("2SepV1", "stringArray ==>" + stringArrayLis.toString());
+                BannerAdepter bannerAdepter = new BannerAdepter(getActivity(), stringArrayLis
+                        , new OnClickItem() {
+                    @Override
+                    public void onClickItem(View view, int i) {
+                        Log.d("22SepV1", "You click at position ==>" + i);
+                        Log.d("22SepV1", "uid Click ==>" + uidStringArrayList.get(i));
+                    }
+                });
+                recyclerView.setAdapter(bannerAdepter);
+
+
+
+
+            }   //for
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
 
 
+    }
 
-    }   //Main Method
+    private void showDisplayName() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser != null) {
+            textView.setText(firebaseUser.getDisplayName());
+            aBoolean = false;
+
+        } else {
+            aBoolean = true;
+        }
+    }
 
 
     private void loginController() {
@@ -63,13 +128,24 @@ public class MainFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-//                Replace Fragment
-                getActivity()
-                        .getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.contentMainFragment, new LoginFragment())
-                        .addToBackStack(null)
-                        .commit();
+                if (aBoolean) {
+
+                    getActivity()
+                            .getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.contentMainFragment, new LoginFragment())
+                            .addToBackStack(null)
+                            .commit();
+                } else {
+
+                    getActivity()
+                            .getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.contentMainFragment, new StudentProfileFragment())
+                            .addToBackStack(null)
+                            .commit();
+
+                }
             }
         });
     }
